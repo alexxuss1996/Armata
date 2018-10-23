324d1ba2641c5a3da1dc47897fafacaffc9717d8
alexxuss1996 <alekseymelnych132@gmail.com>
2018-10-23 11:39:37 +0300

    Section 3 finished

--- app/js/common.js
+++ app/js/common.js
@@ -17,8 +17,9 @@ $(".main-mnu li").click(function () {
 	$(this).toggleClass("active");
 });
 
-$(".section-content .info-item").equalHeights();
 $(".s1-bottom .info-item").equalHeights();
+$(".section_1 .section-content .info-item").equalHeights();
+$(".section_3 .section-content .info-item").equalHeights();
 
 
 
