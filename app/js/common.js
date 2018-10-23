c33b6a5ac3f0e98e20a2479666174d6e7ebc3601
alexxuss1996 <alekseymelnych132@gmail.com>
2018-10-23 15:32:50 +0300

    Section 4 completed

--- app/js/common.js
+++ app/js/common.js
@@ -20,6 +20,16 @@ $(".main-mnu li").click(function () {
 $(".s1-bottom .info-item").equalHeights();
 $(".section_1 .section-content .info-item").equalHeights();
 $(".section_3 .section-content .info-item").equalHeights();
+$(".section_4").waypoint(function() {
+	$(".section_4 .card").each(function(index) {
+		var ths = $(this);
+		setInterval(function() {
+			$(ths).removeClass("card-off").addClass("card-on");
+		}, 150*index)
+	})
+	
+}, { offset: "20%"})
+
 
 
 
